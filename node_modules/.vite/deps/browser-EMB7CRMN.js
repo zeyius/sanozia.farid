56c50db44581b915c4ee1fbe0ce08eec12b6b11a
zeyius <105494282+zeyius@users.noreply.github.com>
2026-02-22 14:25:40 +0000

    still no ai

---
+++ node_modules/.vite/deps/browser-EMB7CRMN.js
@@ -0,0 +1,17 @@
+import {
+  Headers,
+  Request,
+  Response,
+  browser_default,
+  fetch,
+  init_browser
+} from "./chunk-5H4R2CZR.js";
+import "./chunk-V4OQ3NZ2.js";
+init_browser();
+export {
+  Headers,
+  Request,
+  Response,
+  browser_default as default,
+  fetch
+};
