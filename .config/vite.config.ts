0e8be97fd699c0df432f8f6a733959d2b4b141d5
zeyius <105494282+zeyius@users.noreply.github.com>
2026-02-22 15:05:37 +0000

    Add missing Vite/TS config for Netlify build

--- .config/vite.config.ts
+++ .config/vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("../src", import.meta.url)),
-    },
+  build: {
+    outDir: 'dist'
   },
-});
+  server: {
+    port: 5173
+  }
+})
